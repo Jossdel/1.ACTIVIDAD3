d095ed3a4a051f90c628baf8bb5abcedf4c2b6c2
Jossdel <josuedelgadil01@gmail.com>
2025-06-22 21:51:01 -0400

    first commit con ejercicio 1

---
+++ 1.ACTIVIDAD3/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.lstTabla = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(224, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingresa un número:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(416, 76);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(153, 51);
+            this.btnMostrar.TabIndex = 1;
+            this.btnMostrar.Text = "Crear Tabla";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(124, 84);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(286, 35);
+            this.txtNumero.TabIndex = 2;
+            // 
+            // lstTabla
+            // 
+            this.lstTabla.FormattingEnabled = true;
+            this.lstTabla.ItemHeight = 29;
+            this.lstTabla.Location = new System.Drawing.Point(124, 137);
+            this.lstTabla.Name = "lstTabla";
+            this.lstTabla.Size = new System.Drawing.Size(286, 265);
+            this.lstTabla.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstTabla);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.ListBox lstTabla;
+    }
+}
+
---
+++ 1.ACTIVIDAD3/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            lstTabla.Items.Clear(); // Limpiar la lista antes de mostrar nueva tabla
+
+            if (int.TryParse(txtNumero.Text, out int numero))
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    int resultado = numero * i;
+                    lstTabla.Items.Add($"{numero} x {i} = {resultado}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingresa un número válido.");
+            }
+        }
+
+    }
+}
