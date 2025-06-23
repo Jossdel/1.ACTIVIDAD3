63759358ad1caf6e39e3591142b6118ff34d3262
Jossdel <josuedelgadil01@gmail.com>
2025-06-22 22:41:10 -0400

    Ejercicio2

---
+++ 1.ACTIVIDAD3/EJERCICIO 2/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace EJERCICIO_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMinutos = new System.Windows.Forms.TextBox();
+            this.txtSegundos = new System.Windows.Forms.TextBox();
+            this.lblTiempo = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(306, 330);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(165, 71);
+            this.btnIniciar.TabIndex = 0;
+            this.btnIniciar.Text = "btnIniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "\tMinutos:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 230);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Segundos:\n";
+            // 
+            // txtMinutos
+            // 
+            this.txtMinutos.Location = new System.Drawing.Point(142, 165);
+            this.txtMinutos.Name = "txtMinutos";
+            this.txtMinutos.Size = new System.Drawing.Size(100, 35);
+            this.txtMinutos.TabIndex = 3;
+            // 
+            // txtSegundos
+            // 
+            this.txtSegundos.Location = new System.Drawing.Point(142, 230);
+            this.txtSegundos.Name = "txtSegundos";
+            this.txtSegundos.Size = new System.Drawing.Size(100, 35);
+            this.txtSegundos.TabIndex = 4;
+            // 
+            // lblTiempo
+            // 
+            this.lblTiempo.AutoSize = true;
+            this.lblTiempo.Location = new System.Drawing.Point(359, 188);
+            this.lblTiempo.Name = "lblTiempo";
+            this.lblTiempo.Size = new System.Drawing.Size(71, 29);
+            this.lblTiempo.TabIndex = 5;
+            this.lblTiempo.Text = "00:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTiempo);
+            this.Controls.Add(this.txtSegundos);
+            this.Controls.Add(this.txtMinutos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnIniciar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMinutos;
+        private System.Windows.Forms.TextBox txtSegundos;
+        private System.Windows.Forms.Label lblTiempo;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 1.ACTIVIDAD3/EJERCICIO 2/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EJERCICIO_2
+{
+    public partial class Form1 : Form
+    {
+        private int tiempoRestante; // en segundos
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            int minutos, segundos;
+
+            if (!int.TryParse(txtMinutos.Text, out minutos)) minutos = 0;
+            if (!int.TryParse(txtSegundos.Text, out segundos)) segundos = 0;
+
+            tiempoRestante = (minutos * 60) + segundos;
+
+            if (tiempoRestante <= 0)
+            {
+                MessageBox.Show("Por favor, ingresa un tiempo válido.");
+                return;
+            }
+
+            lblTiempo.Text = FormatearTiempo(tiempoRestante);
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            tiempoRestante--;
+
+            lblTiempo.Text = FormatearTiempo(tiempoRestante);
+
+            if (tiempoRestante <= 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("¡Tiempo agotado!");
+            }
+        }
+
+        private string FormatearTiempo(int totalSegundos)
+        {
+            int minutos = totalSegundos / 60;
+            int segundos = totalSegundos % 60;
+            return $"{minutos:D2}:{segundos:D2}";
+        }
+
+
+
+ 
+    }
+}
