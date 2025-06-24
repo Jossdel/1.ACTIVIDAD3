cf343c457044e7f065e6af2cae95a88c1659ce14
Jossdel <josuedelgadil01@gmail.com>
2025-06-23 21:48:39 -0400

    Ejercicio7

---
+++ 1.ACTIVIDAD3/EJERCICIO7/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace EJERCICIO7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInicio = new System.Windows.Forms.TextBox();
+            this.txtFin = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.listBoxPrimos = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInicio
+            // 
+            this.txtInicio.Location = new System.Drawing.Point(220, 94);
+            this.txtInicio.Name = "txtInicio";
+            this.txtInicio.Size = new System.Drawing.Size(154, 35);
+            this.txtInicio.TabIndex = 0;
+            // 
+            // txtFin
+            // 
+            this.txtFin.Location = new System.Drawing.Point(220, 165);
+            this.txtFin.Name = "txtFin";
+            this.txtFin.Size = new System.Drawing.Size(154, 35);
+            this.txtFin.TabIndex = 1;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(64, 231);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(137, 61);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // listBoxPrimos
+            // 
+            this.listBoxPrimos.FormattingEnabled = true;
+            this.listBoxPrimos.ItemHeight = 29;
+            this.listBoxPrimos.Location = new System.Drawing.Point(428, 221);
+            this.listBoxPrimos.Name = "listBoxPrimos";
+            this.listBoxPrimos.Size = new System.Drawing.Size(308, 207);
+            this.listBoxPrimos.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Número inicio:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Número final:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(178, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(521, 48);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Buscador de Numeros Primos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 482);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxPrimos);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtFin);
+            this.Controls.Add(this.txtInicio);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInicio;
+        private System.Windows.Forms.TextBox txtFin;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.ListBox listBoxPrimos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ 1.ACTIVIDAD3/EJERCICIO7/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EJERCICIO7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            listBoxPrimos.Items.Clear(); // Limpiar lista anterior
+
+            // Validar que los campos no estén vacíos
+            if (string.IsNullOrWhiteSpace(txtInicio.Text) || string.IsNullOrWhiteSpace(txtFin.Text))
+            {
+                MessageBox.Show("Por favor, ingresa ambos números.");
+                return;
+            }
+
+            // Convertir los textos a números
+            int inicio, fin;
+            if (!int.TryParse(txtInicio.Text, out inicio) || !int.TryParse(txtFin.Text, out fin))
+            {
+                MessageBox.Show("Por favor, escribe solo números válidos.");
+                return;
+            }
+
+            if (inicio > fin)
+            {
+                MessageBox.Show("El número de inicio debe ser menor o igual al número final.");
+                return;
+            }
+
+            // Buscar números primos en el rango
+            for (int num = inicio; num <= fin; num++)
+            {
+                if (EsPrimo(num))
+                {
+                    listBoxPrimos.Items.Add(num);
+                }
+            }
+        }
+
+        // Función para verificar si un número es primo
+        private bool EsPrimo(int numero)
+        {
+            if (numero <= 1)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(numero); i++)
+            {
+                if (numero % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+    }
+}
