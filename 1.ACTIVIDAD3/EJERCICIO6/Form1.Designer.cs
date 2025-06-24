b8ea101b9385ed91823a062c93d5f665c1ca0bcb
Jossdel <josuedelgadil01@gmail.com>
2025-06-23 21:48:18 -0400

    Ejercicio6

---
+++ 1.ACTIVIDAD3/EJERCICIO6/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace EJERCICIO6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(301, 270);
+            this.maskedTextBox1.Mask = "00000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 35);
+            this.maskedTextBox1.TabIndex = 0;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(269, 337);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(157, 55);
+            this.btnVerificar.TabIndex = 1;
+            this.btnVerificar.Text = "Verificar ";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button btnVerificar;
+    }
+}
+
---
+++ 1.ACTIVIDAD3/EJERCICIO6/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EJERCICIO6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            // Validamos si hay número escrito
+            if (string.IsNullOrWhiteSpace(maskedTextBox1.Text))
+            {
+                MessageBox.Show("Por favor, introduce un número.");
+                return;
+            }
+
+            int numero = int.Parse(maskedTextBox1.Text);
+            int suma = 0;
+
+            // Sumar los divisores propios
+            for (int i = 1; i < numero; i++)
+            {
+                if (numero % i == 0)
+                {
+                    suma += i;
+                }
+            }
+
+            // Verificamos si es perfecto
+            if (suma == numero)
+            {
+                MessageBox.Show(numero + " es un número perfecto.");
+            }
+            else
+            {
+                MessageBox.Show(numero + " no es un número perfecto.");
+            }
+        }
+
+    }
+}
